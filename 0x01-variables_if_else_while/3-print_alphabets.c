06c8864861fb307af7a38a41243c1c01363fe12c
Euryna <majdouline.bastou@gmail.com>
2023-04-21 15:10:15 -0700

    alphabets ul

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabets upper and lower
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
