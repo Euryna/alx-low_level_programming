973838f1495c5fee65288c9b35c8381d1216f598
Euryna <majdouline.bastou@gmail.com>
2023-04-21 16:49:42 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
-		putchar((n % 10) + '0');
-		if (n == 9)
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+		if (n < 9)
 		putchar(',');
-		putchar(' ');
+		putchar(32);
 	putchar('\n');
 	return (0);
 }
